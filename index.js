d7b5f321803d8a7e8ef19d0b166af054032394a4
sinachi ajuzie <Ajuziesinachi@gmail.com>
2023-11-27 02:18:25 +0100

    testing req

--- index.js
+++ index.js
@@ -330,25 +330,25 @@ app.post("/createProfile", async (req, res) => {
     // await users.doc(docId).set(profileData);
     // await users.doc(docId).collection("followers").add(wagmiFollow);
     // await users.doc(docId).collection("following").add(wagmiFollow);
-    const lspFactory = new LSPFactory(provider, {
-      deployKey: privateKey,
-      chainId: 4201,
-    });
+    // const lspFactory = new LSPFactory(provider, {
+    //   deployKey: privateKey,
+    //   chainId: 4201,
+    // });
 
-    const deployedContracts = await lspFactory.UniversalProfile.deploy({
-      controllerAddresses: [ req.body.address ], // root address (address attached to profile)
-      lsp3Profile: {
-        name: req.body.username,
-        description: req.body.bio,
-        tags: ['wagmi-profile'],
-        links: [{
-          title: 'My Website',
-          url: 'www.my-website.com'
-        }]
-      }
-    });
+    // const deployedContracts = await lspFactory.UniversalProfile.deploy({
+    //   controllerAddresses: [ req.body.address ], // root address (address attached to profile)
+    //   lsp3Profile: {
+    //     name: req.body.username,
+    //     description: req.body.bio,
+    //     tags: ['wagmi-profile'],
+    //     links: [{
+    //       title: 'My Website',
+    //       url: 'www.my-website.com'
+    //     }]
+    //   }
+    // });
 
-    const UPAddress = deployedContracts.LSP0ERC725Account.address;
+    // const UPAddress = deployedContracts.LSP0ERC725Account.address;
     // await users.doc(docId).update({ UPAddress: UPAddress });
 
     console.log('success');
